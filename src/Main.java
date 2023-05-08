70328244477abe6543a88aa3b112b7c33e42fc3a
gbiagi <gbiagiharreguy.cf@iesesteveterradas.cat>
2023-05-08 20:12:16 +0200

    Añadidas imagenes de armas y personajes

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,11 @@
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-
-        // Creacion de las armas para la base de datos
-//        Weapon Bow = new Weapon(1,"Bow",5,0,15);
-//        Weapon Axe = new Weapon(2,"Axe",0,3,10);
-//        Weapon DualWieldAxe = new Weapon(3,"Dual Wield Axe",0,5,20);
-//        Weapon Dagger = new Weapon(4,"Dagger",3,0,10);
-//        Weapon Knife = new Weapon(5,"Knife",4,0,12);
-//        Weapon Sword = new Weapon(6,"Sword",1,1,10);
-//        Weapon DualSword = new Weapon(7,"Dual Sword",2,2,14);
-//        Weapon Katana = new Weapon(8,"Katana",3,2,18);
-//        Weapon Scimitar = new Weapon(9,"Scimitar",2,1,14);
-
-        // Creacion de personajes para la base de datos
-
-
 
     }
 }
