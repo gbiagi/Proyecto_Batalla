699f76461ecfddfae267a481adfffa7256c6fa1f
gbiagi <gbiagiharreguy.cf@iesesteveterradas.cat>
2023-05-17 19:16:52 +0200

    update

--- src/BBDDConnection.java
+++ src/BBDDConnection.java
@@ -34,7 +34,7 @@ public class BBDDConnection {
         return rs; // Hacemos return del ResultSet para poder tratar la salida como corresponda.
     }
 
-    public void insertBattle(int BATTLE_ID, int PLAYER_ID, int WARRIOR_ID, int WARRIOR_WEAPON_ID, int OPPONENT_ID,
+    public static void insertBattle(int PLAYER_ID, int WARRIOR_ID, int WARRIOR_WEAPON_ID, int OPPONENT_ID,
                              int OPPONENT_WEAPON_ID, int INJURIES_CAUSED,int INJURIES_SUFFERED,int BATTLE_POINTS) {
 
         String urlDatos = "jdbc:mysql://localhost/BatallaDeRaces?serverTimezone=UTC";
@@ -54,21 +54,56 @@ public class BBDDConnection {
             // 3. Crear una consulta
             //query = "select * from players";
             // 4. Instanciar objeto de la clase consulta
-            String update = "INSERT INTO battle VALUES (?,?,?,?,?,?,?,?,?)";
+            String update = "INSERT INTO battle (PLAYER_ID, WARRIOR_ID, WARRIOR_WEAPON_ID, OPPONENT_ID, OPPONENT_WEAPON_ID," +
+                    " INJURIES_CAUSED, INJURIES_SUFFERED, BATTLE_POINTS) VALUES (?,?,?,?,?,?,?,?,?)";
             PreparedStatement ps = conn.prepareStatement(update);
             // 5. Ejecutar la consulta
 
-            ps.setInt(1,BATTLE_ID);
-            ps.setInt(2,PLAYER_ID);
-            ps.setInt(3,WARRIOR_ID);
-            ps.setInt(4,WARRIOR_WEAPON_ID);
-            ps.setInt(5,OPPONENT_ID);
-            ps.setInt(6,OPPONENT_WEAPON_ID);
-            ps.setInt(7,INJURIES_CAUSED);
-            ps.setInt(8,INJURIES_SUFFERED);
-            ps.setInt(9,BATTLE_POINTS);
+            ps.setInt(1,PLAYER_ID);
+            ps.setInt(2,WARRIOR_ID);
+            ps.setInt(3,WARRIOR_WEAPON_ID);
+            ps.setInt(4,OPPONENT_ID);
+            ps.setInt(5,OPPONENT_WEAPON_ID);
+            ps.setInt(6,INJURIES_CAUSED);
+            ps.setInt(7,INJURIES_SUFFERED);
+            ps.setInt(8,BATTLE_POINTS);
             ps.executeUpdate();
             System.out.println("Battle saved!");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver no se ha cargado correctamente!!");
+        } catch (SQLException e) {
+            System.out.println("Conexion no creada correctamente!!");
+            e.printStackTrace();
+        }
+    }
+    public static void insertPlayer(String username, int score, int enemiesDefeated) {
+        String urlDatos = "jdbc:mysql://localhost/BatallaDeRaces?serverTimezone=UTC";
+        String usuario = "root";
+        String pass = "1234";
+
+        // Pasos a seguir cada vez que queramos usar una base de datos con eclipse
+
+        ResultSet rs = null;
+        try {
+            // 1. Cargar driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            //System.out.println("Driver cargado correctamente");
+            // 2. Crear conexion con la base de datosw
+            Connection conn = DriverManager.getConnection(urlDatos, usuario, pass);
+            //System.out.println("Conexion creada correctamente");
+            // 3. Crear una consulta
+            //query = "select * from players";
+            // 4. Instanciar objeto de la clase consulta
+            String update = "INSERT INTO players (PLAYER_NAME, SCORE, ENEMIES_SLAYED) VALUES (?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(update);
+            // 5. Ejecutar la consulta
+
+            ps.setString(1,username);
+            ps.setInt(2,score);
+            ps.setInt(3,enemiesDefeated);
+            ps.executeUpdate();
+            System.out.println("Player saved!");
 
         } catch (ClassNotFoundException e) {
             System.out.println("Driver no se ha cargado correctamente!!");
--- src/Fight.java
+++ src/Fight.java
@@ -15,6 +15,10 @@ public class Fight extends JFrame {
 	private JLabel playerIcon, botIcon;
 	private JButton characterButton, weaponButton, rankingButton, fightButton, clearButton;
 	private JPanel buttonPanel_1, charactersPanel, characterPanel_1, characterPanel_2, weaponsPanel, statsPanel, statsLvlPanel, buttonPanel_2, consolePanel, mainPanel;
+	private int battleScore = 0;
+	private int totalScore = 0;
+	private int enemiesDefeated = 0;
+	private String username;
 
 	public Fight(Warrior characterChosen, Warrior randomBot) {
 
@@ -60,7 +64,7 @@ public class Fight extends JFrame {
 		fightButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				combat(characterChosen,randomBot);
+				combat(characterChosen, randomBot);
 			}
 		});
 
@@ -95,12 +99,11 @@ public class Fight extends JFrame {
 		weaponButton.setEnabled(false);
 
 		this.setTitle("Menu");
-		this.setLocation(300,0);
+		this.setLocation(300, 0);
 		this.setSize(800, 700);
 		this.setResizable(false);
 		this.setVisible(true);
 	}
-
 
 	//	Method that counts a warrior's total speed
 	public int total_speed(Warrior warrior, Weapon weapon) {
@@ -109,76 +112,71 @@ public class Fight extends JFrame {
 		return count_speed;
 	}
 
-//	Method that counts a warrior's total agility
+	//	Method that counts a warrior's total agility
 	public int total_agility(Warrior warrior) {
 		int count_agility;
 		count_agility = warrior.getAgility();
 		return count_agility;
 	}
 
-//	Method that determines if a warrior can perform an attack or not
+	//	Method that determines if a warrior can perform an attack or not
 	public boolean perform_attack(Warrior warrior) {
 		String message;
 		int num = (int) (Math.floor(Math.random() * 100) + 1);
 		if ((warrior.getAgility() * 10) > num) {
-			message = "\n"+warrior.getName() + ": Attack successfully performed.";
+			message = "\n" + warrior.getName() + ": Attack successfully performed.";
 			text.append(message);
 			return true;
-		}
-		else {
-			message = "\n"+warrior.getName() + ": Attack failed.";
+		} else {
+			message = "\n" + warrior.getName() + ": Attack failed.";
 			text.append(message);
 			return false;
 		}
 	}
 
-//	Method that determines if a warrior can dodge an attack or, in case of receiving it, how much damage recieves
+	//	Method that determines if a warrior can dodge an attack or, in case of receiving it, how much damage recieves
 	public String dodge_attack(Warrior attacker, Weapon weapon, Warrior defender) {
 		String message;
 		int damage = attacker.getStrenght() + weapon.getStrenght() - defender.getDefense();
 		int num = (int) (Math.floor(Math.random() * 50) + 1);
 		if ((defender.getAgility()) > num) {
-			message = defender.getName()+" dodged the attack.";
-		}
-		else {
+			message = defender.getName() + " dodged the attack.";
+		} else {
 			defender.setHealth(defender.getHealth() - damage);
-			message = defender.getName()+" has recieved " + damage + " damage points.";
+			message = defender.getName() + " has recieved " + damage + " damage points.";
 		}
 		return message;
 	}
 
-//	Method that determines if a warrior repeats an attack or gives his turn to his opponent
+	//	Method that determines if a warrior repeats an attack or gives his turn to his opponent
 	public boolean repeat_attack(Warrior attacker, Weapon attackerWeapon, Warrior defender, Weapon defenderWeapon) {
 		boolean repeatAttack;
 		int num = (int) (Math.floor(Math.random() * 100) + 1);
 		if (total_speed(attacker, attackerWeapon) <= total_speed(defender, defenderWeapon)) {
 			repeatAttack = false;
-		}
-		else {
+		} else {
 			if (((total_speed(attacker, attackerWeapon) - total_speed(defender, defenderWeapon)) * 10) > num) {
 				repeatAttack = true;
-				text.append("\n"+attacker.getName()+" attacks again.");
-			}
-			else {
+				text.append("\n" + attacker.getName() + " attacks again.");
+			} else {
 				repeatAttack = false;
 			}
 		}
 		return repeatAttack;
 	}
 
-//	Method that determines who's the warrior that have won
+	//	Method that determines who's the warrior that have won
 	public String winner(Warrior warrior1, Warrior warrior2) {
 		String message = "";
 		if (warrior1.getHealth() <= 0) {
-			message = warrior1.getName() + " wins!";
-		}
-		else if (warrior2.getHealth() <= 0) {
 			message = warrior2.getName() + " wins!";
+		} else if (warrior2.getHealth() <= 0) {
+			message = warrior1.getName() + " wins!";
 		}
 		return message;
 	}
 
-//	Method that determines the fight logic
+	//	Method that determines the fight logic
 	public void combat(Warrior player, Warrior bot) {
 
 		Warrior attacker;
@@ -190,26 +188,21 @@ public class Fight extends JFrame {
 		if (total_speed(player, player.getWeapon()) > total_speed(bot, bot.getWeapon())) {
 			attacker = player;
 			defender = bot;
-		}
-		else if (total_speed(bot, bot.getWeapon()) > total_speed(player, player.getWeapon())) {
+		} else if (total_speed(bot, bot.getWeapon()) > total_speed(player, player.getWeapon())) {
 			attacker = bot;
 			defender = player;
-		}
-		else {
+		} else {
 			if (total_agility(player) > total_agility(bot)) {
 				attacker = player;
 				defender = bot;
-			}
-			else if (total_agility(bot) > total_agility(player)) {
+			} else if (total_agility(bot) > total_agility(player)) {
 				attacker = bot;
 				defender = player;
-			}
-			else {
+			} else {
 				if (num == 1) {
 					attacker = player;
 					defender = bot;
-				}
-				else{
+				} else {
 					attacker = bot;
 					defender = player;
 				}
@@ -217,50 +210,64 @@ public class Fight extends JFrame {
 		}
 		while (true) {
 			while (true) {
-				text.append("\n\n"+attacker.getName()+" turn:");
+				text.append("\n\n" + attacker.getName() + " turn:");
 				//perform_attack(attacker);
-				if (perform_attack(attacker)){
-					text.append("\n"+dodge_attack(attacker, attacker.getWeapon(), defender));
+				if (perform_attack(attacker)) {
+					text.append("\n" + dodge_attack(attacker, attacker.getWeapon(), defender));
 				}
-				text.append("\n"+attacker.getName()+" remaining health: "+attacker.getHealth());
-				text.append("\n"+defender.getName()+" remaining health: "+defender.getHealth());
+				text.append("\n" + attacker.getName() + " remaining health: " + attacker.getHealth());
+				text.append("\n" + defender.getName() + " remaining health: " + defender.getHealth());
 				if (!repeat_attack(attacker, attacker.getWeapon(), defender, defender.getWeapon())) {
 					break;
 				}
 
 			}
 			if (attacker.getHealth() <= 0 || defender.getHealth() <= 0) {
-				text.append("\n*******************************\n"+winner(attacker,defender));
-				if (winner(attacker,defender).contains(player.getName() + " wins!")){
+				text.append("\n*******************************\n" + winner(attacker, defender));
+				if (winner(attacker, defender).equals(player.getName() + " wins!")) {
 					win = true;
-					bot = null;
+				System.out.println(attacker.getName()+"**"+attacker.getHealth());
+				System.out.println(defender.getName()+"**"+defender.getHealth());
+				System.out.println(player.getName()+"**"+player.getHealth());
 				}
-				int selectContinue = JOptionPane.showOptionDialog(mainPanel,"Continue fighting?","Continue?",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,null,null);
-					characterButton.setEnabled(false);
-					weaponButton.setEnabled(false);
+				System.out.println(win);
+				fightButton.setEnabled(false);
+				characterButton.setEnabled(false);
+				weaponButton.setEnabled(false);
+				int selectContinue = JOptionPane.showOptionDialog(mainPanel, "Continue fighting?", "Continue?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+
 				if (selectContinue == 0) {
 					System.out.println("Yes");
+					fightButton.setEnabled(true);
 					characterButton.setEnabled(true);
 					weaponButton.setEnabled(true);
+
 				} else if (selectContinue == 1) {
 					System.out.println("No");
-				}else {
+
+					int maxUserLength = 5; // Maximum number of characters allowed
+					boolean userValid = false;
+					while (!userValid) { // Window asking for a username
+						username = JOptionPane.showInputDialog(mainPanel, "Enter your username (max. 5 characters):");
+						if (username != null && username.length() <= maxUserLength) {
+							userValid = true;
+						} else {
+							JOptionPane.showMessageDialog(mainPanel, "Invalid username. Enter a name with a maximum of 5 characters.");
+						}
+					}
+					JOptionPane.showMessageDialog(mainPanel, "Score saved!");
+					//BBDDConnection.insertBattle();
+					System.exit(0);
+
+				} else {
 					break;
 				}
-
-//				new Continue(player, bot, win, initialHealth).addWindowListener(new WindowAdapter() {
-//					@Override
-//					public void windowClosed(WindowEvent e) {
-//						characterButton.setEnabled(true);
-//						weaponButton.setEnabled(true);
-//					}
-//				});
-//				break;
 			}
 			Warrior aux = attacker;
 			attacker = defender;
 			defender = aux;
-
 		}
 	}
 }
+
+
